1ba55d823483cd49324f3b1ebd659c57c691837f
Shivam Dixit <dixitshivam4499@gmail.com>
2022-12-13 00:51:47 +0530

    Day3 finish.cpp

--- DAY-3/Count Number of Pairs With Absolute Difference K.cpp
+++ DAY-3/Count Number of Pairs With Absolute Difference K.cpp
@@ -3,21 +3,53 @@
 
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int countKDifference(int nums[],int n,int k)
+// {
+//     int count=0;
+//     for(int i=0;i<n;i++)
+//     {
+//         for(int j=i+1;j<n;j++)
+//         {
+//             if(abs(nums[i]-nums[j])==k)
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+
+// int main()
+// {
+//     int nums[]={1,2,2,1},k=1;
+//     int n=sizeof(nums)/sizeof(nums[0]);
+//     cout<<countKDifference(nums,n,k);
+// }
+
+
+
+
+// ********************************************** EFFICIENT SOLUTION ******************************************* //
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int countKDifference(int nums[],int n,int k)
 {
+
+    unordered_map<int,int> mp;
     int count=0;
     for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<n;j++)
-        {
-            if(abs(nums[i]-nums[j])==k)
-            {
-                count++;
-            }
-        }
+        count+=mp[nums[i]+k]+mp[nums[i]-k];
+        mp[nums[i]]++;
     }
     return count;
 }
@@ -25,7 +57,7 @@ int countKDifference(int nums[],int n,int k)
 
 int main()
 {
-    int nums[]={1,2,2,1},k=1;
+    int nums[]={1,2,2,1,4,5,6},k=1;
     int n=sizeof(nums)/sizeof(nums[0]);
     cout<<countKDifference(nums,n,k);
 }
